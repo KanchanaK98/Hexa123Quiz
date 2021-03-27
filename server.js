a67d76f0e64c142c995c2c30ef5c479b79d6fd69
KanchanaK98 <66104442+KanchanaK98@users.noreply.github.com>
2021-03-27 21:38:21 +0530

    fourth

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-/*const path = require('path');
+const path = require('path');
 
 
 //new
@@ -9,7 +9,7 @@ app.get('./api', (req, res) => {
     res.json({
         message: 'This is api endpoint'
     })
-})*/
+})
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
